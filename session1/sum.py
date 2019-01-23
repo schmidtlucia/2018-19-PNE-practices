5ce5fe4c948d4d21c9e157bac1dea4b8168eac18
schmidtlucia <schmidtlucia2000@gmail.com>
2019-01-23 20:52:47 +0100

    Exercise 3, session 1

---
+++ session1/sum.py
@@ -0,0 +1,8 @@
+#Exercise 3 of session 1
+
+print('This program will print the number of the summation of all numbers until 100.')
+
+total = 0
+for i in range(101):
+    total = total + i
+print(total)
