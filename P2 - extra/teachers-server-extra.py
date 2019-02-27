f00c54ee72ce5a83b2d6975ee6a6dba3af6c2f3c
lschmidt <schmidtlucia2000@gmail.com>
2019-02-27 17:58:32 +0100

    This is the extra work of Practice 2 - Server and client with methods reverse and complement

---
+++ P2 - extra/teachers-server-extra.py
@@ -0,0 +1,48 @@
+import socket
+from Seq import Seq
+
+# Configure the Server's IP and PORT
+PORT = 8090
+IP = "212.128.253.88"
+MAX_OPEN_REQUESTS = 5
+
+# Counting the number of connections
+number_con = 0
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    serversocket.bind((IP, PORT))
+    # become a server socket
+    # MAX_OPEN_REQUESTS connect requests before refusing outside connections
+    serversocket.listen(MAX_OPEN_REQUESTS)
+
+    while True:
+        # accept connections from outside
+        print("Waiting for connections at {}, {} ".format(IP, PORT))
+        (clientsocket, address) = serversocket.accept()
+
+        # Another connection!e
+        number_con += 1
+
+        # Print the conection number
+        print("CONNECTION: {}. From the IP: {}".format(number_con, address))
+
+        # Read the message from the client, if any
+        msg = clientsocket.recv(2048).decode("utf-8")
+        print("Message from client: {}".format(msg))
+
+        # Send the message
+        msg_c = Seq(msg)  # We have to convert the message sent from the client into the Seq object
+        msg_s = 'This is the REVERSE of the sequence given: {}\nAnd this is the COMPLEMENT of it: {}'.format(msg_c.reverse().strbases, msg_c.complement().strbases)
+        send_bytes = str.encode(msg_s)
+        # We must write bytes, not a string
+        clientsocket.send(send_bytes)
+        clientsocket.close()
+
+except socket.error:
+    print("Problems using port {}. Do you have permission?".format(PORT))
+
+except KeyboardInterrupt:
+    print("Server stopped by the user")
+    serversocket.close()
