f66083f542cf9630e8a4036e5fe3adad5393f1c6
lschmidt <schmidtlucia2000@gmail.com>
2019-02-27 17:57:28 +0100

    This is the finished Practice 2 - server and client with method reverse

--- P2/client_seq.py
+++ P2/client_seq.py
@@ -2,36 +2,31 @@
 # Practice 2 -session 8
 
 import socket
-
-# CREATING CLASS Seq
-class Seq:
-    """A class for representing sequences"""
-    def __init__(self, str_bases):
-        self.str_bases = str_bases
-    def len(self):
-        return len(self.str_bases)
-    def reverse(self):
+from Seq import Seq
 
 
+# Creating a socket or communicating with the server
 
-#creating a socket or communicating with the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
 
-#same behaviour as a file
-PORT = 8086
+# Same behaviour as a file
+
+PORT = 8080
 IP = '212.128.253.88'
 
-#connect to the server
-s.connect((IP,PORT))
+# Connect to the server
+
+s.connect((IP, PORT))
 
 condition = True
 
 while condition:
-#send a message to the server
-    msg2 = input('Message: ')
-    msg2.lower()
-    s.send(str.encode(msg2))  # encode is to translate the string into bytes
+
+    # Send a message to the server
+    msg2 = Seq(input('Write a DNA-sequence: ').upper())
+    msg_rev = str(msg2.reverse().strbases)  # I need to write '.strbases' so the program reads it out of the Seq class and converts it into a str
+    s.send(str.encode(msg_rev))  # encode is to translate the string into bytes
     if msg2 == 'exit':
         condition = False
         s.close()
@@ -39,4 +34,3 @@ while condition:
         msg = s.recv(2048).decode('utf-8')
         print('Message from the server: ')
         print(msg)
-
