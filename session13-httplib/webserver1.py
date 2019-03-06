5aac1eb041fa12f249a31ab84bad7fb6b43750a1
lschmidt <schmidtlucia2000@gmail.com>
2019-03-06 17:59:30 +0100

    session 13 - http library (done)

--- session13-httplib/webserver1.py
+++ session13-httplib/webserver1.py
@@ -1,7 +1,7 @@
 import http.server
 import socketserver
 
-PORT = 8001
+PORT = 8006
 
 # a Handler is a class. We call it whenever there is a request from a client
 
@@ -14,12 +14,32 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         print("     Cmd: "+self.command)
         print("     Path: "+self.path)
 
-        # Create a message, in this case, the happy server
-        content = "I am the happy server! :-)"
+        req_line = self.requestline.split(" ")
+        if req_line[1] == '/':
+            file = open('index.html', 'r')
+            content = file.read()
+            file.close()
+        elif req_line[1] == '/pink.html':
+            file = open('pink.html', 'r')
+            content = file.read()
+            file.close()
+        elif req_line[1] == '/blue.html':
+            file = open('blue.html', 'r')
+            content = file.read()
+            file.close()
+        elif req_line[1] == '/green.html':
+            file = open('green.html', 'r')
+            content = file.read()
+            file.close()
+        else:
+            file = open('error.html', 'r')
+            content = file.read()
+            file.close()
+
 
         #  special code 200 means that everything is okay
         self.send_response(200)
-        self.send_header('Content-Type', 'text/plain')
+        self.send_header('Content-Type', 'text/html')
         self.send_header('Content-Length', len(str.encode(content)))
         self.end_headers()
 
