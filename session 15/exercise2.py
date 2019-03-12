b64fb5f6038a28bbe0023933e0241b7582dd1aa7
lschmidt <schmidtlucia2000@gmail.com>
2019-03-12 20:00:37 +0100

    exercise 2 of session 15 - must be done: print message in 'form5.html'

---
+++ session 15/exercise2.py
@@ -0,0 +1,63 @@
+# This is Exercise 2 of the session 15
+
+import http.server
+import socketserver
+import termcolor
+
+PORT = 8080
+
+
+class TestHandler(http.server.BaseHTTPRequestHandler):  # We are creating objects that heritates the properties of the http.server library
+
+    def do_GET(self):
+
+        # -- printing the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        f = open('form4.html', 'r')
+        contents = f.read()
+
+        # -- creating a happy server response
+        self.send_response(200)
+
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('  Content-Length', len(str.encode(contents)))
+        self.end_headers()
+
+        #  create a response different from happy server
+        req_line = self.requestline.split(" ")[1]
+        if req_line != '':
+            if req_line == '/':
+                file = open('form4.html', 'r')
+                content = file.read()
+            elif req_line == '/form4.html':
+                file = open('form4.html', 'r')
+                content = file.read()
+            elif req_line == '/echo?msg=':
+                file = open('form4.html', 'r')
+                content = file.read()
+            else:
+                msg = req_line.partition('=')
+                if msg[0] == '/echo?msg':
+                    file = open('form5.html', 'r')
+                    content = file.read()
+                else:
+                    file = open('error-ex2.html', 'r')
+                    content = file.read()
+            self.wfile.write(str.encode(content))
+
+# -- MAIN PROGRAM
+
+
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:  # an empty IP adress means: 'use your own IP'
+    print('Serving at PORT {}'.format(PORT))
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
