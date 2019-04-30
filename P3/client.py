98bfbddd72d06ffb18c7447f6be21d8a8b05bfcd
lschmidt <schmidtlucia2000@gmail.com>
2019-04-30 15:22:55 +0200

    P3 done

--- P3/client.py
+++ P3/client.py
@@ -2,35 +2,29 @@
 # Practice 2 -session 8
 
 import socket
-from Seq import Seq
 
-
-# Creating a socket or communicating with the server
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
-
-# Same behaviour as a file
+# SERVER IP, PORT
 
 PORT = 8080
-IP = '212.128.253.106'
+IP = "212.128.253.105"
 
-# Connect to the server
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# establish the connection to the Server (IP, PORT)
 s.connect((IP, PORT))
 
-condition = True
+msg = "ACTGATCAT\nlen\ncomplement"
+if msg == "":
+    msg = "hey"
 
-while condition:
 
-    # Send a message to the server
-    msg2 = Seq(input('Write a DNA-sequence: ').upper())
-    msg_rev = str(msg2.reverse().strbases)  # I need to write '.strbases' so the program reads it out of the Seq class and converts it into a str
-    s.send(str.encode(msg_rev))  # encode is to translate the string into bytes
-    if msg2 == 'exit':
-        condition = False
-        s.close()
-    else:  # receive a message from the server
-        msg = s.recv(2048).decode('utf-8')
-        print('Message from the server: ')
-        print(msg)
+# Send the request message to the server
+s.send(str.encode(msg))
+
+# Receive the servers response
+response = s.recv(2048).decode()
+
+# Print the server's response
+print(response)
+
+s.close()
--- P3/server.py
+++ P3/server.py
@@ -5,46 +5,91 @@ from Seq import Seq
 
 # Configure the Server's IP and PORT
 PORT = 8080
-IP = "212.128.253.106"
+IP = "212.128.253.105"
 MAX_OPEN_REQUESTS = 5
 
-# Counting the number of connections
-number_con = 0
 
-# create an INET, STREAMing socket
+def operations(cli):
+
+    # Reading the message from the client
+    message = cli.recv(2048).decode("utf-8")
+    message = message.split('\n')
+
+    sequence = Seq(message[0])
+    nucleicbases = "ACTG"
+
+    for i in message[1:]:
+        if i != "len" and i != "complement" and i != "reverse" and i != "countA" and i != "countC" and i != "countG" \
+                and i != "countT" and i != "percA" and i != "percC" and i != "percG" and i != "percT":
+            final = "Error"
+            cli.send(str.encode(final))
+            return
+
+    if message[0] == "hey":
+        final = "ALIVE"
+        cli.send(str.encode(final))
+        return
+
+    counter = 0
+    for n in message[0].upper():
+        if n in nucleicbases:
+            counter += 1
+            if counter == len(message[0]):
+                final = "OK!\n"
+            elif counter != len(message[0]):
+                final = "Error"
+        cli.send(str.encode(final))
+        return
+
+    for oper in message[1:]:
+        print("Message from the client: {}".format(oper))
+        if oper == "len":
+            final = str(sequence.len()) + '\n'
+        elif oper == "complement":
+            final = str(sequence.complement().strbases) + "\n"
+        elif oper == "reverse":
+            final = str(sequence.reverse().strbases) + "\n"
+        elif oper == "countA":
+            final = str(sequence.count("A")) + "\n"
+        elif oper == "countC":
+            final = str(sequence.count("C")) + "\n"
+        elif oper == "countG":
+            final = str(sequence.count("G")) + "\n"
+        elif oper == "countT":
+            final = str(sequence.count("T")) + "\n"
+        elif oper == "percA":
+            final = str(sequence.perc("A")) + "%\n"
+        elif oper == "percC":
+            final = str(sequence.perc("C")) + "%\n"
+        elif oper == "percG":
+            final = str(sequence.perc("G")) + "%\n"
+        elif oper == "percT":
+            final = str(sequence.perc("T")) + "%\n"
+
+
+# Sending the message back to the client
+        # (because we are an echo server)
+    cli.send(str.encode(final))
+
+
+# Create a socket for connecting to the client
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    serversocket.bind((IP, PORT))
-    # become a server socket
-    # MAX_OPEN_REQUESTS connect requests before refusing outside connections
-    serversocket.listen(MAX_OPEN_REQUESTS)
 
-    while True:
-        # accept connections from outside
-        print("Waiting for connections at {}, {} ".format(IP, PORT))
-        (clientsocket, address) = serversocket.accept()
+serversocket.bind((IP, PORT))
 
-        # Another connection!e
-        number_con += 1
+serversocket.listen(MAX_OPEN_REQUESTS)
 
-        # Print the conection number
-        print("CONNECTION: {}. From the IP: {}".format(number_con, address))
+print("Socket ready: {}".format(serversocket))
 
-        # Read the message from the client, if any
-        msg = clientsocket.recv(2048).decode("utf-8")
-        print("Message from client: {}".format(msg))
+while True:
 
-        # Send the message
-        msg_c = Seq(msg)  # We have to convert the message sent from the client into the Seq object
-        msg_s = 'This is the LENGTH of the sequence given: {}\n, this is the # of A\'s: {}, C\'s: {}, G\'s: {} and T\'s: {} of it; and this itś percentage respectively: {}, {}, {} and{}.: {}'.format(msg_c.len().strbases, msg_c.count('A').strbases, msg_c.count('C').strbases, msg_c.count('G').strbases, msg_c.count('T').strbases, msg_c.perc('A').strbases, msg_c.perc('C').strbases, msg_c.perc('G').strbases, msg_c.perc('T').strbases)
-        send_bytes = str.encode(msg_s)
-        # We must write bytes, not a string
-        clientsocket.send(send_bytes)
-        clientsocket.close()
+    print("Waiting or connections at: {}, {}".format(IP, PORT))
+    (clientsocket, address) = serversocket.accept()
 
-except socket.error:
-    print("Problems using port {}. Do you have permission?".format(PORT))
+    # -- Process the client request
+    print("Attending client: {}".format(address))
 
-except KeyboardInterrupt:
-    print("Server stopped by the user")
-    serversocket.close()
+    operations(clientsocket)
+
+    # -- Process the client request
+    clientsocket.close()
